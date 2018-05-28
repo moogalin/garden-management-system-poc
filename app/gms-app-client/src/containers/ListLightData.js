590e7e3385ba9e59d42f25084e18d6727891552e
chavissabin1 <sabinc@oregonstate.edu>
2018-05-28 16:23:52 -0700

    Updated graph and sorted data
    updated graph to show data within the last 144 datapoints (~24 hours)
    and sorted data by date.

--- app/gms-app-client/src/containers/ListLightData.js
+++ app/gms-app-client/src/containers/ListLightData.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import { API } from "aws-amplify";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { timeParse as parse } from 'd3-time-format';
 import {AreaChart} from 'react-easy-chart';
 import "./ListLightData.css";
 import './react-bootstrap-table-all.min.css';
+import moment from 'moment';
 
 export default class ListLightData extends Component {
   constructor(props) {
@@ -14,17 +16,33 @@ export default class ListLightData extends Component {
       isLoading: true,
       lightdata: []
     };
+    
+    this.graphData = []
+    this.chartData = []
   }
 
+  
+  
   async componentDidMount() {
   if (!this.props.isAuthenticated) {
     return;
   }
 
   try {
-    const lightdata = await this.getLightData();
-    this.setState({ lightdata });
-    console.log(JSON.stringify(this.state));
+    var lightdata = await this.getLightData();
+    this.setState({ lightdata });    
+    var tempData =  lightdata
+    
+    var date_sort_asc = function (data1, data2) {
+        var date1 = data1.Time, date2 = data2.Time;
+    
+        if (date1 > date2) return 1;
+        if (date1 < date2) return -1;
+        return 0;
+    };
+    
+    var storedData = tempData.sort(date_sort_asc);
+    
   } catch (e) {
     alert(e);
   }
@@ -38,69 +56,106 @@ getLightData() {
     body: data
   }).catch(error => {
     console.log(error.response)
-  });
+  });  
 }
 
 renderLightGraph(data){
-//check data validity, if using
-  if (data !== undefined) {
-    var count = data.length;
-    //var data = data.slice(count-10,count);
-    var data2 = data.forEach( data =>
-      data.Time = new Date(data.Time).toLocaleString("en-US")
-    );
-  }
+    if(data != undefined) {
+        this.graphData = []
+        var count = data.length;
+        
+        const parseDate = parse('%d-%b-%y %H:%M');
+        var tempData = data.slice(count-144,count);
+        var dataIR = [];
+        var dataFS = [];
+        var dataV = [];
+        var i
+        for (i = 0; i < tempData.length; i++) {
+            var tempDate = tempData[i].Time.replace("T"," ").substring(0,16)
+            let date = moment(tempDate, 'YYYY-MM-DD HH:mm');
+            
+            var subData = { x: date.format('D-MMM-YY HH:mm'), y: tempData[i]["IR"]}
+            var subfs = { x: date.format('D-MMM-YY HH:mm'), y: tempData[i]["Full_Spectrum"] }
+            var subv = { x: date.format('D-MMM-YY HH:mm'), y: tempData[i]["Visible"] }
+            dataIR.push(subData);
+            dataFS.push(subfs);
+            dataV.push(subv)
+        }
+        this.graphData.push(dataIR)
+        this.graphData.push(dataFS)
+        this.graphData.push(dataV)
+        console.log(JSON.stringify(this.graphData));
+    }
+
 //return HTML of what I want to display
   return(
+  <div>
       <AreaChart
-        // datePattern={'%H:%M'}
-        xType={'text'}
+        data={this.graphData}
+        // xType={'text'}
+        datePattern={'%d-%b-%y %H:%M'}
+        xType={'time'}
         axes
-        yDomainRange={[0, 100]}
+        yDomainRange={[0, 70000]}
         grid
-        areaColors={['orange', 'purple']}
+        areaColors={['orange', 'purple', 'green']}
         interpolate={'cardinal'}
         width={750}
-        height={300}
-        axisLabels={{x: 'Hour', y: '%'}}
-        style={{ '.label': { fill: 'black' } }}
-        data={[
-          [
-            { x: '12AM', y: 20 },
-            { x: '1AM', y: 10 },
-            { x: '2AM', y: 85 },
-            { x: '3AM', y: 45 },
-            { x: '4AM', y: 15 }
-          ], [
-            { x: '12AM', y: 10 },
-            { x: '1AM', y: 15 },
-            { x: '2AM', y: 13 },
-            { x: '3AM', y: 90 },
-            { x: '4AM', y: 10 }
-          ]
-        ]}
+        height={320}
+        margin={{top: 0, right: 0, bottom: 50, left: 65}}
+        axisLabels={{x: 'Time', y: 'LUX'}}
+        style={'.label'}
       />
+        <legend></legend>
+        <table class = "legendFormat">
+            <th class = "Legend">Legend</th>
+            <tr>
+                <td class="Legend">Infrared = </td>
+                <td><span class="IRdot"></span></td>
+                <td class="Space"></td>
+                <td class="Legend">Full Spectrum = </td>
+                <td><span class="FSdot"></span></td>
+                <td class="Space"></td>
+                <td class="Legend">Visible = </td>
+                <td><span class="Vdot"></span></td>
+                <td class="Space"></td>
+            </tr>
+        </table>
+        <br />
+        </div>
     );
 }
 
 renderLightDataList(data) {
   if (data != undefined) {
-    var count = data.length;
-    var data = data.slice(count-10,count);
-    var data2 = data.forEach( data =>
-      data.Time = new Date(data.Time).toLocaleString("en-US")
-    );
+     var count = data.length;
+    this.chartData = []
+    var tempData = data.slice(count-144,count);
+    var i
+      //console.log(JSON.stringify(tempData));
+     for (i = 0; i < tempData.length; i++) {
+         var subtime = new Date(tempData[i].Time).toLocaleString("en-US")
+         
+        this.chartData.push({
+             Sensor: tempData[i].Sensor, 
+             userID: tempData[i].userID, 
+             Visible: tempData[i].Visible, 
+             IR: tempData[i].IR, 
+             Full_Spectrum: tempData[i].Full_Spectrum, 
+             MAC: tempData[i].MAC, 
+             Time: subtime});
+      }
   }
   return (
     <div>
         <div>
           {"Total Datapoints: " + count
-            + " Viewing: " + (count < 10 ? count : 10)
+            + " Viewing: " + (count < 144 ? count : 144)
           }
         <br/>
           {"Note: Only sensor data associated to logged in user is viewable"}
         </div>
-        <BootstrapTable data={data} striped hover>
+        <BootstrapTable data={this.chartData} striped hover>
           <TableHeaderColumn dataField='Sensor' isKey={true} hidden={true}>Sensor</TableHeaderColumn>
           <TableHeaderColumn dataField='Time'>Time</TableHeaderColumn>
           <TableHeaderColumn dataField='IR'>IR</TableHeaderColumn>
@@ -126,8 +181,8 @@ renderLightData() {
     <div className="LightData">
       <PageHeader>Your Light Sensor Data</PageHeader>
       <div>
-        {this.renderLightDataList(this.state.lightdata)}
         {this.renderLightGraph(this.state.lightdata)}
+        {this.renderLightDataList(this.state.lightdata)}
       </div>
     </div>
   );
