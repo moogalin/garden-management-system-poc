18ac65826bad4997bf7e2b2d2b6f33bef86bfc55
Megan Aldridge <m.aldridge@live.com>
2018-06-02 19:23:53 -0400

    Created API for Temperature Data

---
+++ app/gms-app-api/gettemperaturedata.js
@@ -0,0 +1,29 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: "Temperature_Data.dev",
+    IndexName: "MAC-sorted-by-userId-index",
+    // 'KeyConditionExpression' defines the condition for the query
+    // 'ExpressionAttributeValues' defines the value in the condition
+    KeyConditionExpression: "MAC = :MAC and userId = :userId",
+    //FilterExpression: "userId = :userId",
+    ExpressionAttributeValues: {
+      ":MAC": data.MAC,
+      ":userId": event.requestContext.identity.cognitoIdentityId
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("query", params);
+    console.log("RESULT:" + JSON.stringify(result));
+    // Return the matching list of items in response body
+    callback(null, success(result.Items));
+  } catch (e) {
+    console.log(e);
+    callback(null, failure({ status: false }));
+  }
+}
