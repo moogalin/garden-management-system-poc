a4d3162329dde35a257c26d108acb4926f78ebf2
Megan Aldridge <m.aldridge@live.com>
2018-06-03 02:23:31 -0400

    Updated New Plant component to use utility function for dropdown data generation

--- app/gms-app-client/src/containers/NewPlant.js
+++ app/gms-app-client/src/containers/NewPlant.js
@@ -4,6 +4,7 @@ import { FormGroup, FormControl, ControlLabel, Col } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
 import config from "../config";
 import "./NewPlant.css";
+import { dropdown } from "../utils/Dropdown";
 
 export default class NewNote extends Component {
   constructor(props) {
@@ -15,24 +16,42 @@ export default class NewNote extends Component {
       qty: "",
       date: "",
       age: "",
-      sunlight: ""
+      sunlight: "",
+      mac_list:[],
+      mac: ""
     };
   }
 
+  async componentDidMount() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
+
+    try {
+      const mac_list = await this.sensor();
+      this.setState({ mac_list });
+    } catch (e) {
+      alert(e);
+    }
+
+    this.setState({ isLoading: false });
+  }
+
+  sensor() {
+    return API.get("plants", "pis/claimed");
+  }
+
   validateForm() {
+
+    console.log("MAC IS: " + this.state.mac_assigned);
     return this.state.name.length > 0;
   }
 
   handleChange = event => {
+    console.log("id: " + event.target.id);
+    console.log("value: " + event.target.value);
     this.setState({
       [event.target.id]: event.target.value
-    });
-  }
-
-  handleDropdownChange = event => {
-    var index = event.nativeEvent.target.selectedIndex;
-    this.setState({
-      [event.target.id]: event.nativeEvent.target[index].text
     });
   }
 
@@ -47,7 +66,8 @@ export default class NewNote extends Component {
         qty: this.state.qty,
         date: this.state.date,
         age: this.state.age,
-        sunlight: this.state.sunlight
+        sunlight: this.state.sunlight,
+        MAC: this.state.mac_assigned
       });
       this.setState({ isLoading: false });
       alert("Sucess! Plant added");
@@ -64,6 +84,31 @@ export default class NewNote extends Component {
     }).catch(error => {
       console.log(error.response)
     });
+  }
+
+  dropdownAssociateSensor(data) {
+
+    //const list = data.map((entry) =>
+      //<option
+      //key={entry.MAC.toString()}
+      //value = {entry.MAC}> {entry.MAC} </option>
+    //);
+
+    const test = dropdown(data, "MAC");
+
+    //console.log(list);
+
+    return (
+      <FormControl
+        componentClass="select"
+        placeholder="select"
+        value={this.state.mac_assigned}
+        onChange={this.handleChange}
+      >
+      //<option value="blank"></option>
+      { test }
+      </FormControl>
+    );
   }
 
   render() {
@@ -118,7 +163,7 @@ export default class NewNote extends Component {
                 value={this.state.age}
                 onChange={this.handleChange}
               >
-                <option valie="blank"></option>
+                <option value="blank"></option>
                 <option value="Seed">Seed</option>
                 <option value="Starter">Starter</option>
                 <option value="transplant">Transplant</option>
@@ -137,11 +182,27 @@ export default class NewNote extends Component {
                 value={this.state.sunlight}
                 onChange={this.handleChange}
               >
-                <option valie="blank"></option>
+                <option value="blank"></option>
                 <option value="Full">Full</option>
                 <option value="Partial">Partial</option>
                 <option value="Shade">Shade</option>
               </FormControl>
+            </Col>
+          </FormGroup>
+        </div>
+        <div className="ProfileFormGroup">
+          <FormGroup controlId="mac_assigned" bsSize="small">
+          <Col componentClass={ControlLabel} sm={3}>Associate A Sensor (Pi MAC address)</Col>
+            <Col sm={9}>
+                <FormControl
+                  componentClass="select"
+                  placeholder="select"
+                  value={this.state.mac_assigned}
+                  onChange={this.handleChange}
+                >
+                <option value="blank"></option>
+                { dropdown(this.state.mac_list, "MAC") }
+                </FormControl>
             </Col>
           </FormGroup>
         </div>
