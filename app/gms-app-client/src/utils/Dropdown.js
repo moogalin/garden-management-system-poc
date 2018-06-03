4d1d6605d64cf9ce16bb2734d0fe14f299b0fc45
Megan Aldridge <m.aldridge@live.com>
2018-06-03 02:52:00 -0400

    Added dynamic disable dropdown creation

--- app/gms-app-client/src/utils/Dropdown.js
+++ app/gms-app-client/src/utils/Dropdown.js
@@ -5,7 +5,25 @@ export function dropdown (data, item)  {
   const list = data.map((entry) =>
     <option
     key={entry[item].toString()}
-    value = {entry[item]}> {entry[item]} </option>
+    value = {entry[item]}>
+      {entry[item]}
+    </option>
+  );
+
+  console.log(list);
+
+  return list;
+}
+
+export function dropdownDisabled (data, item)  {
+
+  const list = data.map((entry) =>
+    <option
+    key={entry[item].toString()}
+    value = {entry[item]}
+    disabled>
+      {entry[item]}
+    </option>
   );
 
   console.log(list);
