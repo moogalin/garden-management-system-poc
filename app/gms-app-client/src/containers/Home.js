1d0e3aa298c75492a12aa915cc0b77e67358e3c9
valefleur <valefleur@gmail.com>
2018-06-07 21:51:40 -0700

    Removed Latin placeholder from home screen.

--- app/gms-app-client/src/containers/Home.js
+++ app/gms-app-client/src/containers/Home.js
@@ -11,11 +11,8 @@ export default class Home extends Component {
           <h1>Garden Management System</h1>
           <p><b>Note: This website is inacessible without authenticated user account. Login or sign up now</b></p>
           <br/>
-          <br/>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mollis malesuada congue. Curabitur dictum enim ut placerat euismod. Sed eu venenatis ex, et feugiat urna. In ut blandit tellus. Praesent id eleifend est. Nunc viverra risus a sollicitudin auctor. Sed convallis dui erat. Praesent eget ligula laoreet, convallis erat nec, mattis leo. Nunc in nisi congue, commodo tortor eget, sagittis justo. Mauris mollis magna in justo mattis, eu blandit lacus suscipit. Quisque ut elementum dui. Pellentesque dignissim mi est, quis volutpat odio mollis vitae.
-          </p>
           < p >
-            Praesent rhoncus ligula nec nulla placerat, sit amet euismod mauris blandit.Donec felis sapien, pretium et venenatis et, porttitor sed libero.Morbi maximus mollis egestas.Nullam aliquet tortor eu imperdiet interdum.Fusce elementum urna eget urna porta vulputate.Aenean eget libero eget urna consectetur faucibus nec vitae mauris.Morbi sed dolor ac nulla viverra euismod sed sed justo.Nulla in auctor orci.Nulla facilisi.Pellentesque placerat nulla vitae iaculis tristique.Maecenas at ipsum vehicula, malesuada velit id, pretium dolor.Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+          Welcome to the Garden Management System!  This system uses sensors, small computers, and the cloud to track your garden.  Metrics can be viewed online from anywhere, even Mars!  Sign in now!
           </p>
         </div>
       </div>
